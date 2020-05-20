09b7bb77b1bb9a3b62dd1d54f76a9417af6c3c5f
Lucas Morais <lucas.moraissz07@gmail.com>
2020-05-20 16:46:59 -0300

    add: initializing burguer builder container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
-
+import BurguerBuilder from './containers/BurguerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          Alo
+          <BurguerBuilder></BurguerBuilder>
         </Layout>
       </div>
     );
