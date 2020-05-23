861ca37eb368959113b5dbafca449784996bdd4d
Lucas Morais <lucas.moraissz07@gmail.com>
2020-05-23 12:10:33 -0300

    add: adding message when the burger has 0 ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,26 +3,34 @@ import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = (props) =>{
-    const ingredients = Object.keys(props.ingredients).map((igName) =>{
-        return [...Array(props.ingredients[igName])].map((_,index)=>{
-            
-            return <BurgerIngredient type= {igName} key ={igName + index}></BurgerIngredient>
-            
+const burger = (props) => {
+    let ingredients = Object.keys(props.ingredients).map((igName) => {
+        return [...Array(props.ingredients[igName])].map((_, index) => {
+
+            return <BurgerIngredient type={igName} key={igName + index}></BurgerIngredient>
+
         })
-        })
-    
+    }).reduce((acc, actualElement) => {
+
+        return acc.concat(actualElement)
+
+    }, [])
+
+    if(ingredients.length === 0){
+        ingredients = <p> Please start adding ingredients!</p>
+    }
+
     console.log(ingredients)
 
-    
+
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
-                {ingredients}
-                
+            {ingredients}
+
             <BurgerIngredient type='bread-bottom'></BurgerIngredient>
-        </div>        
+        </div>
     )
 }
 
