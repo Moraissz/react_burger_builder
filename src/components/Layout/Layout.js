46d85163a34328f6e0fdc04d78c4d7186262c76e
Lucas Morais <lucas.moraissz07@gmail.com>
2020-06-03 15:58:47 -0300

    add: add the backdrop to the side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <SideDrawer></SideDrawer>
-        <Toolbar></Toolbar>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+    state={
+        showSideDrawer: true
+    }
 
-export default layout
+    sideDrawerClosedHandler = () =>{
+        this.setState({
+            showSideDrawer:false
+        })
+
+    }
+
+    
+    render() {
+        return (
+            <Aux>
+                <SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} ></SideDrawer>
+                <Toolbar></Toolbar>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import classes from './SideDrawer.css'
+import Aux from '../../../hoc/Aux'
 
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const sideDrawer = () => {
+
+const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer,classes.Close]
+
+    if(props.show)
+        attachedClasses = [classes.SideDrawer,classes.Open]
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}> 
-                <Logo></Logo>
-            </div>
-            
-            <nav>
-                <NavigationItems></NavigationItems>
-            </nav>
+        <Aux>
+            <Backdrop visible={props.show} clicked={props.closed}></Backdrop>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo></Logo>
+                </div>
 
-        </div>
+                <nav>
+                    <NavigationItems></NavigationItems>
+                </nav>
+
+            </div>
+        </Aux>
     )
 
 }
