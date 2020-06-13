c9949618bda4cde3811aa3875bb4cca0fdcd5fe9
Lucas Morais <lucas.moraissz07@gmail.com>
2020-06-13 13:50:12 -0300

    refactor: handling the user to the checkout page  when finish the burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -113,34 +113,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
-        this.setState({ loading: true })
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            costumer: {
-                name: 'Lucas',
-                address: {
-                    street: 'Av Barão louco',
-                    zipCode: '444-332',
-                    country: 'Brazil'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-
-        axios.post('/orders.json', order).then((response) => (
-            this.setState({
-                loading: false,
-                purchasing: false
-            })
-        )).catch((error) => {
-            this.setState({
-                loading: false,
-                purchasing: false
-            })
-        })
+       this.props.history.push('/checkout')
 
     }
 
