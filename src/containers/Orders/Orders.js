d39b78bf1a82b439a053e3bbd9eecea818e54f3a
Lucas Morais <lucas.moraissz07@gmail.com>
2020-06-15 18:12:49 -0300

    add: handling errors on orders page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import axios from '../../axios-orders'
 
 import Order from '../../components/Order/Order'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component{
 
@@ -47,4 +48,4 @@ class Orders extends Component{
 
 }
 
-export default Orders
+export default withErrorHandler(Orders,axios)
