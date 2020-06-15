ebbbfa0cb07c58b34d51610a8375b0c6d19af283
Lucas Morais <lucas.moraissz07@gmail.com>
2020-06-15 17:16:18 -0300

    add: create contact data and submitting the order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,21 +2,33 @@ import React, {Component} from 'react'
 
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import { Route } from 'react-router'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component{
 
     state ={
-        ingredients:{}
+        ingredients:null,
+        price: 0
     }
 
-    componentDidMount(){
+    componentWillMount(){
         const query = new URLSearchParams(this.props.location.search)
         const ingredients = {}
+        let price = 0
         for(let param of query.entries()){
-            ingredients[param[0]] = Number(param[1])
+
+            if(param[0] === 'price'){
+                price = param[1]
+
+            }else{
+                ingredients[param[0]] = Number(param[1])
+            }
+            
         }
         this.setState({
-            ingredients
+            ingredients,
+            price
         })
     }
 
@@ -36,7 +48,9 @@ class Checkout extends Component{
                 <CheckoutSummary ingredients={this.state.ingredients} 
                 checkoutContinued={this.checkoutContinuedHandler} 
                 checkoutCancelled={this.checkoutCancelledHandler} ></CheckoutSummary>
+                <Route path={this.props.match.path + '/contact-data'} render={() => <ContactData ingredients={this.state.ingredients} price={this.state.price}/>} />
             </div>
+            
         )
     }
     
