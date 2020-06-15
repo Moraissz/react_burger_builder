1f1fb2b317f9ec19a7da2843d64b44c335bed9d2
Lucas Morais <lucas.moraissz07@gmail.com>
2020-06-15 17:46:50 -0300

    add: fetching orders data from firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,35 @@
 import React, {Component} from 'react'
+import axios from '../../axios-orders'
 
 import Order from '../../components/Order/Order'
 
 class Orders extends Component{
+
+    state = {
+        loading:true
+    }
+
+    componentDidMount(){
+        axios.get('/orders.json').then((res) =>{
+            this.setState({
+                loading:false
+            })
+            const fetechedOrders = []
+            for(let key in res.data){
+                fetechedOrders.push({
+                    id: key,
+                    ...res.data[key]
+                })
+            }
+            console.log(fetechedOrders)
+
+        }).catch((error) =>{
+            this.setState({
+                loading:false
+            })
+
+        })
+    }
 
     render(){
         return(
