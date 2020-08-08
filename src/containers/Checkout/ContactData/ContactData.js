e1951759b50ad67a7518b6ddf45f6ec9c6235a4e
Lucas Morais <lucas.moraissz07@gmail.com>
2020-08-08 13:50:45 -0300

    add : adding all order process to redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,8 @@ import classes from './ContactData.css'
 
 import axios from '../../../axios-orders'
 import {connect} from 'react-redux'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions/index'
 
 import Button  from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner'
@@ -84,13 +86,12 @@ class ContactData extends Component {
                         {value:'cheapest', displayValue:'Cheapest'}
                     ]
                 },
-                value:'',
+                value:'fastest',
                 rules:{},
                 valid:true
             },            
         },
-        formIsValid:false,
-        loading:false
+        formIsValid:false
     }
 
     orderHandler = (event) =>{
@@ -111,17 +112,7 @@ class ContactData extends Component {
 
        
 
-        axios.post('/orders.json',order).then((response) => {
-            this.setState({
-                loading:false
-            })
-            this.props.history.push('/')
-        }).catch((error) => {
-            this.setState({
-                loading:false,
-            })
-        }) 
-
+        this.props.onOrderBurger(order)
     }
 
     checkValidity = (value,rules) => {
@@ -196,7 +187,7 @@ class ContactData extends Component {
                         <Button btnType={'Success'} disabled={!this.state.formIsValid} clicked={this.orderHandler}> ORDER</Button>
                     </form>)
 
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner/>
         }
 
@@ -212,9 +203,17 @@ class ContactData extends Component {
 
 const mapStateToProps = (state) =>{
     return{
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading:state.order.loading,
     }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)) 
+    }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(ContactData,axios));
--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore, compose,applyMiddleware} from 'redux';
+import {createStore, compose,applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order'
 
+
+const rootReducer = combineReducers({
+    burgerBuilder:burgerBuilderReducer,
+    order: orderReducer
+})
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(burgerBuilderReducer,composeEnhancers(
+
+const store = createStore(rootReducer,composeEnhancers(
     applyMiddleware(thunk)
 ));
 
