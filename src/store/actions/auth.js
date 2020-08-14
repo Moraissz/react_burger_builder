3fadc247c167055656446ef81c37aa1fa423aea9
Lucas Morais <lucas.moraissz07@gmail.com>
2020-08-14 18:14:26 -0300

    add: sign up users

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actions from './actionTypes'
-
+import axios from 'axios'
 
 const authStart = () => {
     return {
@@ -24,5 +24,21 @@ const authFail = (error) => {
 export const auth = (email,password) => {
     return (dispatch) => {
         dispatch(authStart())
+        const authData = {
+            email,
+            password,
+            returnSecureToken:true
+        }
+        
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAKSwRRTwi614PjcoR0w15CF_1bL-ZdeOs',authData)
+        .then((response)=>{
+            console.log(response.data)
+            dispatch(authSuccess(response.data))
+
+        }).catch((error) =>{
+            console.log(error)
+            dispatch(authFail(error))
+
+        })
     }
 }
