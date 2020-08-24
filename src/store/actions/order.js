8a144bd1f02112702f915dba74bb9843e2b6e9f0
Lucas Morais <lucas.moraissz07@gmail.com>
2020-08-24 17:18:26 -0300

    add: requiring token to see and make orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -30,10 +30,10 @@ export const purchaseInit = () =>{
 
 }
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData,token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart())
-        axios.post('/orders.json', orderData).then((response) => {
+        axios.post('/orders.json?auth=' + token, orderData).then((response) => {
             dispatch(purchaseBurgerSuccess(response.data.name,orderData))
         
         }).catch((error) => {
@@ -61,10 +61,10 @@ const fetchOrdersFail = () =>{
     }
 }
 
-export const fetchOrders = () =>{
+export const fetchOrders = (token) =>{
     return dispatch => {
         dispatch(fetchOrdersStart())
-        axios.get('/orders.json').then((res) =>{
+        axios.get('/orders.json?auth=' + token).then((res) =>{
             const fetchedOrders = []
             for(let key in res.data){
                 fetchedOrders.push({
